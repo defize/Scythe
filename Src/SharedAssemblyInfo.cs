b9ce50d3a114992b3cb1c139735a6b862c09b084
Adam Thornburn <adam.thornburn@defize.co.uk>
2011-10-30 19:17:05 +0000

    Initial commit

---
+++ Src/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Defize")]
+[assembly: AssemblyProduct("Defize Scythe")]
+[assembly: AssemblyCopyright("Copyright © Defize 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
