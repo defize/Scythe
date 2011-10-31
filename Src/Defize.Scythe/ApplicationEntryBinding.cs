14fb7484006bf23b3fdcdedd61e2da3e9f1f6ddf
Adam Thornburn <adam.thornburn@defize.co.uk>
2011-10-31 22:27:21 +0000

    Parsing binding expression

--- Src/Defize.Scythe/ApplicationEntryBinding.cs
+++ Src/Defize.Scythe/ApplicationEntryBinding.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq.Expressions;
+    using System.Reflection;
 
     public class ApplicationEntryBinding<T, T2>
     {
@@ -13,7 +14,24 @@
         public ApplicationEntryBindingAliases To(Expression<Func<T, Action<T2>>> binding)
         {
             Console.WriteLine(binding.Body);
+            DetermineMethod(binding);
             return new ApplicationEntryBindingAliases();
+        }
+
+        private void DetermineMethod(Expression<Func<T, Action<T2>>> binding)
+        {
+            var ue = (UnaryExpression)binding.Body;
+
+            var mce = (MethodCallExpression)ue.Operand;
+
+            var argument = (ConstantExpression)mce.Arguments[2];
+            var methodInfo = (MethodInfo)argument.Value;
+
+            if (methodInfo.DeclaringType != typeof(T) || methodInfo.GetParameters().Length != 1 || methodInfo.GetParameters()[0].ParameterType != typeof(T2))
+            {
+                throw new Exception();
+            }
+            Console.WriteLine(methodInfo.Name);
         }
     }
 }
